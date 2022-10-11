18636aaf5ce0db1021f8be63c001046ebb60ae35
Benoit-Catta <csupremenight@gmail.com>
2022-10-11 14:16:08 +0200

    tp end

--- movie/movie.py
+++ movie/movie.py
@@ -1,21 +1,42 @@
 from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
 from ariadne.constants import PLAYGROUND_HTML
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 
 import resolvers as r
 
 PORT = 3001
-HOST = '0.0.0.0'
+HOST = 'localhost'
 app = Flask(__name__)
 
-### ADD THINGS HERE
-#
-###
+type_defs = load_schema_from_path('movie.graphql')
+
+movie = ObjectType('Movie')
+actor = ObjectType('Actor')
+movieData = ObjectType('MovieData')
+
+
+query = QueryType()
+query.set_field('movie_with_id', r.movie_with_id)
+query.set_field('actor_with_id', r.actor_with_id)
+query.set_field('GetMovieByTitle', r.GetMovieByTitle)
+query.set_field('GetListMovies', r.GetListMovies)
+
+mutation = MutationType()
+mutation.set_field('update_movie_rate', r.update_movie_rate)
+mutation.set_field('DeleteByMovieId', r.DeleteByMovieId)
+mutation.set_field('PostMovie', r.PostMovie)
+
+movie.set_field('actors', r.resolve_actors_in_movie)
+#movieData.set_field('movies', r.resolve_movies_in_movieData)
+
+
+schema = make_executable_schema(type_defs, movie, query, mutation, actor, movieData)
 
 # root message
 @app.route("/", methods=['GET'])
 def home():
-    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
+    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>", 200)
+
 
 #####
 # graphql entry points
@@ -23,19 +44,16 @@ def home():
 @app.route('/graphql', methods=['GET'])
 def playground():
     return PLAYGROUND_HTML, 200
-    
+
+
 @app.route('/graphql', methods=['POST'])
 def graphql_server():
     data = request.get_json()
-    success, result = graphql_sync(
-    schema,
-    data,
-    context_value=None,
-    debug=app.debug
-    )
+    success, result = graphql_sync(schema, data, context_value=None, debug=app.debug)
     status_code = 200 if success else 400
     return jsonify(result), status_code
 
+
 if __name__ == "__main__":
-    print("Server running in port %s"%(PORT))
-    app.run(host=HOST, port=PORT)
+    print("Server running in port %s" % (PORT))
+    app.run(host=HOST, port=PORT, debug=True)
--- movie/resolvers.py
+++ movie/resolvers.py
@@ -1,2 +1,77 @@
 import json
 
+def movie_with_id(_,info,_id):
+    with open('{}/data/movies.json'.format("."), "r") as file:
+        movies = json.load(file)
+        for movie in movies['movies']:
+            if movie['id'] == _id:
+                return movie
+def GetListMovies(_,info):
+    print("GetListMovies", _, info)
+    with open('{}/data/movies.json'.format("."), "r") as file:
+        movies = json.load(file)
+        return movies
+
+
+def PostMovie(_,info,_id, _title,_director,_rating):
+    with open('{}/data/movies.json'.format("."), "r") as file:
+        movies = json.load(file)
+        movies.append({"title": _title, "rating": _rating, "director": _director, "id": _id})
+    with open('{}/data/movies.json'.format("."), "w") as wfile:
+        json.dump(movies, wfile)
+
+    for movie in movies['movies']:
+        if movie['id'] == _id:
+            return movie
+
+def DeleteByMovieId(_,info,_id):
+    with open('{}/data/movies.json'.format("."), "r") as file:
+        movies = json.load(file)
+        newMovies = []
+        for movie in movies['movies']:
+            if movie['id'] == _id:
+                movieDel = movie
+            else:
+                newMovies.append(movie)
+    with open('{}/data/movies.json'.format("."), "w") as wfile:
+        json.dump(newMovies, wfile)
+    return movieDel
+
+def GetMovieByTitle(_,info,_title):
+    with open('{}/data/movies.json'.format("."), "r") as file:
+        movies = json.load(file)
+        for movie in movies['movies']:
+            if movie['title'] == _title:
+                return movie
+
+# def resolve_movies_in_movieData(movieData, info):
+#     with open('{}/data/movies.json'.format("."), "r") as file:
+#         movies = json.load(file)
+#         print(movies['movies'])
+#         return movies['movies']
+def update_movie_rate(_,info,_id,_rate):
+    newmovies = {}
+    newmovie = {}
+    with open('{}/data/movies.json'.format("."), "r") as rfile:
+        movies = json.load(rfile)
+        for movie in movies['movies']:
+            if movie['id'] == _id:
+                movie['rating'] = _rate
+                newmovie = movie
+                newmovies = movies
+    with open('{}/data/movies.json'.format("."), "w") as wfile:
+        json.dump(newmovies, wfile)
+    return newmovie
+
+def resolve_actors_in_movie(movie, info):
+    with open('{}/data/actors.json'.format("."), "r") as file:
+        data = json.load(file)
+        actors = [actor for actor in data['actors'] if movie['id'] in actor['films']]
+        return actors
+
+def actor_with_id(_,info,_id):
+    with open('{}/data/actors.json'.format("."), "r") as file:
+        data = json.load(file)
+        for actor in data['actors']:
+            if actor["id"] == _id:
+                return actor
